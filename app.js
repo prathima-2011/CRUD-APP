2f941408612cee9fc802ea3d311899ff38e18fe4
Prathima Dodda <prathima_dodda@endpoint-10-192-72-241.ufvisitor.ufl.edu>
2026-04-16 16:04:57 -0400

    Removed credentials by adding dotenv

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express()
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://*****/myapp")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err));
 
@@ -43,6 +44,6 @@ app.delete("/users/:id", (req, res) => {
     res.send("Users Deleted..!")
 });
 */
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
